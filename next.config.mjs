652d7231c54e4cd0b01a22064e000efab09af6db
eslamsemedo <eslamsemedo@gmail.com>
2025-10-08 17:52:05 +0300

    image handle in next

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['placehold.co', 'res.cloudinary.com'],
+  },
+};
 
 export default nextConfig;
