c740686e2ebdf4d4bc125c6998a8b8a8cf35e532
eslamsemedo <eslamsemedo@gmail.com>
2025-10-15 12:58:45 +0300

    refactor: remove ReduxTripsProvider wrapper from dashboard layout

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -3,11 +3,11 @@ import AdminHeader from "@/components/admin/adminHeader";
 import ReduxTripsProvider from '@/components/Provides/reduxProvider'
 export default function dashboardLayout({ children }) {
   return (
-    <ReduxTripsProvider>
+    <>
       <AdminHeader />
       <div className="bg-black min-h-screen">
         {children}
       </div>
-    </ReduxTripsProvider>
+    </>
   );
 }
