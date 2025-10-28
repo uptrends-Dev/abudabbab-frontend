627765c3f4efc23c0560257fddb58d3ddc47429a
reyad <reyadmohamed631@gmail.com>
2025-10-28 18:04:39 +0300

    ...

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -3,8 +3,8 @@ import { redirect } from "next/navigation";
 import AdminHeader from "../../components/admin/adminHeader";
 import { whoisme } from "../../lib/apis/api";
 import { logout } from "../../lib/apis/authApi";
-import CallbackLogOut from "../../components/utils/callbackLogOut";
 import Adminpro from "../../components/admin/Adminpro";
+import CallBackLogOut from "../../components/utils/CallBackLogOut";
 async function getUserData() {
   try {
     await whoisme();
@@ -29,7 +29,7 @@ export default async function DashboardLayout({ children }) {
 
   if (!authed) {
     // You could also just: redirect("/dashboard/login");
-    return <CallbackLogOut />;
+    return <CallBackLogOut/>
   }
 
   return (
